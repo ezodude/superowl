5f8bb8b31a199e543bd56bb0c966e102bf77599c
Abdel A Saleh <asaleh@the-experimenters.com>
2011-05-20 14:06:02 +0100

    Minor message updates and added capistrano deployment.

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+require "bundler/capistrano"
+
+set :application, "smugowl"
+
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+set :repository, "git@github.com:the-experimenters/smugowl.me.git"  # Your clone URL
+set :scm, "git"
+
+set :user, "deploy"  # The server's user for deploys
+#set :use_sudo, false # avoid using the root user
+ssh_options[:port] = 22
+ssh_options[:username] = 'deploy'
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+ssh_options[:paranoid] = false
+
+set :branch, "master"
+set :scm_verbose, true
+set :deploy_via, :remote_cache
+
+set :deploy_to, "/var/www/#{application}"
+server '109.74.193.80', :web, :app, :db, :primary => true
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
