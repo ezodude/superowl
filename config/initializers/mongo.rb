32d0eb9231aacdb2455b21a4aefe4778e201ade9
Abdel A Saleh <asaleh@the-experimenters.com>
2011-05-22 20:41:37 +0100

    Added correct bkgd image. Integrated MongoHQ for Heroku deployment.

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,13 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "superowl-#{Rails.env}"
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+else
+  MongoMapper.config = {RAILS_ENV => {'uri' => "mongodb://localhost/superowl-#{Rails.env}"}}
+end
+
+MongoMapper.connect(RAILS_ENV)
+
+# MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# MongoMapper.database = "superowl-#{Rails.env}"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
