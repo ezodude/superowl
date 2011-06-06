679007e3ca70f9a55cbdc6d3653636d4c0f67ad2
Abdel A Saleh <asaleh@the-experimenters.com>
2011-06-07 00:37:10 +0100

    Adding Gmail verification.

--- app/controllers/interested_users_controller.rb
+++ app/controllers/interested_users_controller.rb
@@ -3,7 +3,8 @@ class InterestedUsersController < ApplicationController
     interested_user = InterestedUser.new(params[:interested_user])
     
     if interested_user.save
-      flash[:notice] = "Thanks for registering. We're in Pre-Alpha & we'll get in touch as soon as we're ready."
+      #InterestedUserMailer.welcome_email(interested_user).deliver
+      flash[:notice] = "Super Owl is hatching and we're keeping you in the loop :)"
     else
       flash[:error] = "#{interested_user.errors[:email].first}!"
     end
