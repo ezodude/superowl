e85f9710dc14a75acae5fc589e42dd8543b492eb
Abdel A Saleh <asaleh@the-experimenters.com>
2011-10-18 14:19:38 +0100

    Updated home page super owl phone image. Updated email templates to reflect current stage and branding.

--- app/mailers/interested_user_mailer.rb
+++ app/mailers/interested_user_mailer.rb
@@ -5,6 +5,6 @@ class InterestedUserMailer < ActionMailer::Base
     @user = interested_user
     @url  = (RAILS_ENV == "production" ? "http://superowl.me" : "http://0.0.0.0:3000")
     mail(:to => interested_user.email,
-         :subject => "SuperOwl - Thanks for Signing Up for Updates!")
+         :subject => "Super Owl - Thanks for Signing Up for Updates!")
   end
 end
