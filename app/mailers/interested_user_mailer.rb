90f4d8bf7841ee1e677fa59715825d93a7537ccf
Abdel A Saleh <asaleh@the-experimenters.com>
2012-02-10 00:16:17 +0000

    Updated the mailer copy.

--- app/mailers/interested_user_mailer.rb
+++ app/mailers/interested_user_mailer.rb
@@ -3,7 +3,7 @@ class InterestedUserMailer < ActionMailer::Base
   
   def welcome_email(interested_user)
     @user = interested_user
-    @url  = (RAILS_ENV == "production" ? "http://superowl.me" : "http://0.0.0.0:3000")
+    @url  = (RAILS_ENV == "production" ? "http://www.thesupertimes.com/" : "http://0.0.0.0:3000")
     mail(:to => interested_user.email,
          :subject => "Super Owl - Thanks for Signing Up for Updates!")
   end
