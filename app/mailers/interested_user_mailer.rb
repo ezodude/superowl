ca43cbe4b3b6d823caf167db66fecd8a7cdb75e3
Abdel A Saleh <asaleh@the-experimenters.com>
2012-02-10 01:04:48 +0000

    Minor email template updates.

--- app/mailers/interested_user_mailer.rb
+++ app/mailers/interested_user_mailer.rb
@@ -1,10 +1,10 @@
 class InterestedUserMailer < ActionMailer::Base
-  default :from => "aisha@superowl.me"
+  default :from => "aisha@thesupertimes.com"
   
   def welcome_email(interested_user)
     @user = interested_user
     @url  = (RAILS_ENV == "production" ? "http://www.thesupertimes.com/" : "http://0.0.0.0:3000")
     mail(:to => interested_user.email,
-         :subject => "Super Owl - Thanks for Signing Up for Updates!")
+         :subject => "The Super Times, Sign Up Confirmation")
   end
 end
