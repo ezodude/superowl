c8b66d0b44bf9f218021b227b116129e60cf80eb
Abdel Saleh <abdel.a.saleh@gmail.com>
2012-12-21 17:40:44 +0000

    Removed interested user functionality.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @interested_user = InterestedUser.new
-    @interested_user.email ||= "email..."
-    
     respond_to do |format|
       format.html  # new.html.erb
-      format.json  { render :json => @interested_user }
     end
   end  
 end
