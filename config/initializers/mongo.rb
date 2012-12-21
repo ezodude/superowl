37e160434faa489b6c402cf4e1fc340eeaeca303
Abdel Saleh <abdel.a.saleh@gmail.com>
2012-12-21 17:52:21 +0000

    Removed the need to run mongo.

--- config/initializers/mongo.rb
+++
@@ -1,9 +0,0 @@
-# TODO: Remove this file
-
-if ENV['MONGOHQ_URL']
-  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
-else
-  MongoMapper.config = {RAILS_ENV => {'uri' => "mongodb://localhost/superowl-#{Rails.env}"}}
-end
-
-MongoMapper.connect(RAILS_ENV)
